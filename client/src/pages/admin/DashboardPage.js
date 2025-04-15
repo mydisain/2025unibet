5b687076d4a8a830b78dd38363c50ae6a73b193e
mydisain <info@mydisain.com>
2025-04-15 10:05:44 +0300

    Eemalda Timeslot pealkiri ja tõlked

--- client/src/pages/admin/DashboardPage.js
+++ client/src/pages/admin/DashboardPage.js
@@ -26,48 +26,8 @@ const DashboardPage = () => {
         {/* Main Timeslot View */}
         <Grid item xs={12}>
           <Paper sx={{ p: 2 }}>
-            <Typography variant="h6" gutterBottom>
-              {t('timeslot_view', 'Timeslot View')}
-            </Typography>
             <AdminTimeslotView />
           </Paper>
-        </Grid>
-
-        {/* Quick Stats */}
-        <Grid item xs={12} md={4}>
-          <Card>
-            <CardHeader title={t('today_bookings', "Today's Bookings")} />
-            <CardContent>
-              <Typography variant="h3" align="center">
-                {/* This would be connected to real data in a full implementation */}
-                0
-              </Typography>
-            </CardContent>
-          </Card>
-        </Grid>
-
-        <Grid item xs={12} md={4}>
-          <Card>
-            <CardHeader title={t('available_karts', 'Available Karts')} />
-            <CardContent>
-              <Typography variant="h3" align="center">
-                {/* This would be connected to real data in a full implementation */}
-                9
-              </Typography>
-            </CardContent>
-          </Card>
-        </Grid>
-
-        <Grid item xs={12} md={4}>
-          <Card>
-            <CardHeader title={t('total_bookings', 'Total Bookings')} />
-            <CardContent>
-              <Typography variant="h3" align="center">
-                {/* This would be connected to real data in a full implementation */}
-                0
-              </Typography>
-            </CardContent>
-          </Card>
         </Grid>
       </Grid>
 
